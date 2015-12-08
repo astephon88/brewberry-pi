2270b6264905c48ee59f0db91eca63e81a80fe2e
Andrew Stephon <astephon88@gmail.com>
2015-12-07 22:50:23 -0500

    Changed temperature dislays to jQuery knobs. added code to change set temperature from the webpage

--- www/keg/set_temp.php
+++ www/keg/set_temp.php
@@ -3,18 +3,22 @@ $db_host = 'localhost';
 $db_user = 'root';
 $db_pwd = 'beermenu';
 $database = 'beer';
+
+$new_set_temp = $_GET['new_set_temp'];
+
 if (!mysql_connect($db_host, $db_user, $db_pwd))    
 	die("Can't connect to database");
 if (!mysql_select_db($database))    
 	die("Can't select database");
 
-// sending query
-$result = mysql_query("SELECT value FROM temps WHERE type='current';");
-if (!$result) {    
+$query_format = "UPDATE temps SET value=%s WHERE Type='setting';";
+$query = sprintf($query_format,$new_set_temp);
+
+
+$result = mysql_query($query);
+if (!$result) {
 	die("Query to show fields from table failed");
 }
-echo mysql_fetch_array($result,MYSQL_NUM)[0];
-  
-
 mysql_free_result($result);
+
 ?>
